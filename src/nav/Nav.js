a931720e238be997a08e8111b42ccf9e7975539f
Hugh <u88803494@gmail.com>
2020-02-23 22:18:12 +0800

    'test'

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -59,11 +59,11 @@ class TheNavbar extends React.Component {
         fixed="top"
         className={isHidden && "navbar--hide"}
       >
-        <Navbar.Brand href="/#">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="/#">React-Blog</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="/#">首頁</Nav.Link>
+            <Nav.Link href="/#index">首頁</Nav.Link>
             <Nav.Link href="/#posts">文章列表</Nav.Link>
             <Nav.Link href="/#about">關於我</Nav.Link>
           </Nav>
